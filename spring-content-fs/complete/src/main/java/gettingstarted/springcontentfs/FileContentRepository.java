61ea8c5bdba0dcd1ecfb56f8c9616706fb0c5e07
Paul Warren <paul.warren@emc.com>
2016-08-17 17:13:15 +0100

    Add initial and complete projects for the Getting Started with Spring Content guide

---
+++ spring-content-fs/complete/src/main/java/gettingstarted/springcontentfs/FileContentRepository.java
@@ -0,0 +1,6 @@
+package gettingstarted.springcontentfs;
+
+import com.emc.spring.content.commons.repository.ContentStore;
+
+public interface FileContentRepository extends ContentStore<File, String> {
+}
---
+++ spring-content-fs/complete/src/main/java/gettingstarted/springcontentfs/SpringContentFsApplication.java
@@ -0,0 +1,12 @@
+package gettingstarted.springcontentfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContentFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContentFsApplication.class, args);
+	}
+}
