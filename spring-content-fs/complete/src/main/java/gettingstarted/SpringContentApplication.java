1653b80cce68d45fedaa2a9f32f4140ecc2946ce
Paul Warren <paul.warren@emc.com>
2017-05-05 08:58:09 -0700

    Set a custom s3 endpoint

--- spring-content-fs/complete/src/main/java/gettingstarted/SpringContentApplication.java
+++ spring-content-fs/complete/src/main/java/gettingstarted/SpringContentApplication.java
@@ -3,16 +3,25 @@ package gettingstarted;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.content.s3.config.AbstractS3ContentRepositoryConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
 
 @SpringBootApplication
 public class SpringContentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringContentApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringContentApplication.class, args);
+		
+		/**
+		 * Set a custom endpoint
+		 * 
+		 * At the moment this has to be done after everything else has been initialized
+		 */
+		context.getBean(AmazonS3.class).setEndpoint("http://some/endpoint");
 	}
 	
 	@Configuration
