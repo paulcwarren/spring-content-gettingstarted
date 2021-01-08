b3104d44aa673d8fe6ab3eda58d1f41d09efef0b
Paul Warren <paul_c_warren@yahoo.com>
2021-01-07 20:29:52 -0800

    Use an eventually to give elastic time to index
    - otherwise test is flaky

--- spring-content-with-fulltext/complete/src/test/java/gettingstarted/GettingStartedTest.java
+++ spring-content-with-fulltext/complete/src/test/java/gettingstarted/GettingStartedTest.java
@@ -1,7 +1,20 @@
 package gettingstarted;
 
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
-import com.jayway.restassured.RestAssured;
+import static com.github.grantwest.eventually.EventuallyLambdaMatcher.eventuallyEval;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.time.Duration;
+
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,14 +22,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.ByteArrayInputStream;
-import java.util.List;
-
-import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+import com.jayway.restassured.RestAssured;
 
 @RunWith(Ginkgo4jSpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,17 +31,17 @@ public class GettingStartedTest {
 
 	@Autowired private FileRepository fileRepo;
 	@Autowired private FileContentStore fileContentStore;
-	
+
     @Value("${local.server.port}") private int port;
 
     private File file1, file2;
-    
+
     {
         Describe("Fulltext Tests", () -> {
         	BeforeEach(() -> {
         		RestAssured.port = port;
         	});
-        	
+
         	Context("given a File Entity with content", () -> {
         		BeforeEach(() -> {
         			file1 = new File();
@@ -47,17 +54,22 @@ public class GettingStartedTest {
             		file2 = fileContentStore.setContent(file2, new ByteArrayInputStream("Hello Spring Content World!".getBytes()));
 					file2 = fileRepo.save(file2);
         		});
-        		
+
         		It("should be searchable", () -> {
-					given()
-						.config(RestAssured.config()
-								.encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false)))
-						.header("accept", "application/hal+json")
-						.get("/files/searchContent?queryString=Content")
-						.then()
-						.statusCode(HttpStatus.SC_OK)
-						.assertThat().body(not(containsString("/files/" + file1.getId())))
-						.assertThat().body(containsString("/files/" + file2.getId()));
+
+                    assertThat(() -> {
+                        return given()
+                                .config(RestAssured.config()
+                                .encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+                                .header("accept", "application/hal+json")
+                            .get("/files/searchContent?queryString=Content")
+                            .then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .extract().body().asString();
+                    },  eventuallyEval(allOf(
+                                           not(containsString("/files/" + file1.getId())),
+                                           containsString("/files/" + file2.getId())),
+                                       Duration.ofSeconds(5)));
 				});
         	});
         });
