4ffb0d4b2a9ea8d4e05b2f0e29cf16f951dab426
Paul Warren <paul_c_warren@yahoo.com>
2022-02-07 21:31:40 -0800

    Refactor renditions getting started guide to use a content property

--- spring-content-with-renditions/complete/src/main/java/gettingstarted/File.java
+++ spring-content-with-renditions/complete/src/main/java/gettingstarted/File.java
@@ -1,17 +1,19 @@
 package gettingstarted;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.content.commons.annotations.ContentId;
-import org.springframework.content.commons.annotations.ContentLength;
-import org.springframework.content.commons.annotations.MimeType;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
+
+import org.springframework.content.commons.annotations.ContentId;
+import org.springframework.content.commons.annotations.ContentLength;
+import org.springframework.content.commons.annotations.MimeType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -28,5 +30,5 @@ public class File {
 
 	@ContentId private String contentId;
 	@ContentLength private long contentLength;
-	@MimeType private String mimeType;
+	@MimeType private String contentMimeType;
 }
--- spring-content-with-renditions/complete/src/test/java/gettingstarted/GettingStartedTest.java
+++ spring-content-with-renditions/complete/src/test/java/gettingstarted/GettingStartedTest.java
@@ -1,7 +1,17 @@
 package gettingstarted;
 
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
-import com.jayway.restassured.RestAssured;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.io.ByteArrayInputStream;
+
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.ByteArrayInputStream;
-
-import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
-import static org.hamcrest.CoreMatchers.*;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+import com.jayway.restassured.RestAssured;
 
 @RunWith(Ginkgo4jSpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,31 +28,31 @@ public class GettingStartedTest {
 
 	@Autowired private FileRepository fileRepo;
 	@Autowired private FileContentStore fileContentStore;
-	
+
     @Value("${local.server.port}") private int port;
 
     private File file1;
-    
+
     {
         Describe("Rendition Tests", () -> {
         	BeforeEach(() -> {
         		RestAssured.port = port;
         	});
-        	
+
         	Context("given a File Entity with content", () -> {
         		BeforeEach(() -> {
         			file1 = new File();
-					file1.setMimeType("text/plain");
+					file1.setContentMimeType("text/plain");
 					file1 = fileContentStore.setContent(file1, new ByteArrayInputStream("Hello Spring World!".getBytes()));
 					file1 = fileRepo.save(file1);
         		});
-        		
+
         		It("should be renderable", () -> {
 					given()
 						.config(RestAssured.config()
 								.encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false)))
 						.header("accept", "image/jpeg")
-						.get("/files/" + file1.getId())
+						.get("/files/" + file1.getId() + "/content")
 						.then()
 						.statusCode(HttpStatus.SC_OK)
 						.assertThat().body(is(not(nullValue())));
